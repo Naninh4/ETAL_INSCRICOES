ef792adbe3b72b05f4580a2589ec060db6c3c1a6
Naninh4 <liviaeusla@gmail.com>
2022-11-12 22:54:38 -0300

    versão um

---
+++ inscricao/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from inscricao.models import Candidato
+# Register your models here.
+admin.site.register(Candidato)
---
+++ inscricao/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from inscricao.forms import CandidatoForm
+from inscricao.models import Candidato
+# Create your views here.
+
+
+def get_cadastros(request):
+    if request.method == 'POST':
+        form = CandidatoForm(request.POST) #Armazenando o formulário criado a uma lista
+        if form.is_valid():
+            print(request.POST)
+            alter_form = form.save(commit=False)
+            alter_form.mini_cursos = " | ".join(dict(request.POST)['mini_cursos'])
+
+            alter_form.save()
+
+            return redirect("/")
+        else:
+            form = CandidatoForm
+
+    forms =  CandidatoForm
+    candidato = Candidato.objects.all()
+
+    return render(request, "cadastro.html", {"forms": forms, "candidato": candidato})
+
+# def get_post_method(request):
+
+#   if request.method == "POST":
+
+#     form = AlunoForm(request.POST)
+
+#     if form.is_valid():
+
+#       alter_form = form.save(commit=False)
+#       alter_form.gender = " | ".join(dict(request.POST)['gender'])
+      
+#       alter_form.save()
+      
+#       return redirect("/")
+
+#   forms = AlunoForm
+#   aluno = Aluno.objects.all() 
+
+#   return render(request, "get.html", {"forms": forms, "aluno": aluno})
